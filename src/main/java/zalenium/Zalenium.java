3a0c65ca8efd30ae531d38031ea249c405e81340
Rubens Lobo <rubens.lobo@gft.com>
2020-05-14 21:25:09 -0300

    Alterado ambiente

--- src/main/java/zalenium/Zalenium.java
+++ src/main/java/zalenium/Zalenium.java
@@ -1,21 +1,12 @@
 package zalenium;
 
-import azure.controller.CucumberController;
-import azure.controller.LoginController;
-import azure.model.testresult.ResultTestCase;
-import azure.model.testresult.Results;
 import driver.DriverManager;
-import io.restassured.http.ContentType;
-import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import lombok.extern.log4j.Log4j2;
 import support.Verifications;
 
-import java.util.Collections;
-import java.util.List;
-
 import static io.restassured.RestAssured.given;
-import static javafx.application.Platform.exit;
+
 
 @Log4j2
 public class Zalenium extends DriverManager {
@@ -29,7 +20,6 @@ public class Zalenium extends DriverManager {
             Verifications.wait(1);
             if (timeout == configuration.timeout()) {
                 log.error("Execedeu o time out para disponibilizar o servidor do zalenium");
-                exit();
             }
             timeout++;
         }
